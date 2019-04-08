886fb6f999db6a91c44ceb0e1d87b407b88e65d0
Gabriel Huecas <gabriel@dit.upm.es>
2019-04-08 13:19:16 +0200

    REMOVED: python-pip was not needed ADDED: clear comments CHANGED: order to be executed

--- install2.sh
+++ install2.sh
@@ -1,32 +1,36 @@
-
 #using ubuntu t2.xlarge 32G storage  ( 2 minutes execution)
 
+#
+# UPDATE the system
 sudo apt-get -y update
 sudo apt-get -y upgrade
 
+#
+# Install git and wget
 sudo apt-get -y install git
-sudo apt-get -y install wget 
+sudo apt-get -y install wget
 
-
-sudo apt -y install python-pip
-
-
-wget http://kdd.ics.uci.edu/databases/kddcup99/kddcup.data.gz
-wget http://kdd.ics.uci.edu/databases/kddcup99/kddcup.data_10_percent.gz
-
-gunzip kddcup.*.gz
-
-
-wget https://repo.continuum.io/archive/Anaconda2-5.1.0-Linux-x86_64.sh 
-sudo sh Anaconda2-5.1.0-Linux-x86_64.sh -b 
+#
+# Download and install Anaconda 3
+wget https://repo.continuum.io/archive/Anaconda2-5.1.0-Linux-x86_64.sh
+sudo sh Anaconda2-5.1.0-Linux-x86_64.sh -b
 
 source ~/.bashrc
 
 sudo anaconda2/bin/conda update -y -n  base conda
 
+#
+# Download and uncompress the datasets
+wget http://kdd.ics.uci.edu/databases/kddcup99/kddcup.data.gz
+wget http://kdd.ics.uci.edu/databases/kddcup99/kddcup.data_10_percent.gz
+
+gunzip kddcup.*.gz
+
+#
+# Download and execute the ML script
 git clone https://github.com/ghuecas/kdd99ml
 
 
 source ~/.bashrc
-./anaconda2/bin/python kdd99ml/detectAttack.py 
+./anaconda2/bin/python kdd99ml/detectAttack.py
 
