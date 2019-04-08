bc24cee130f06d23a768d6122449c203819a71b6
Gabriel Huecas <gabriel@dit.upm.es>
2019-04-08 11:12:28 +0200

    UPDATED: to python 3.7, mainly print sentences

--- detectAttack.py
+++ detectAttack.py
@@ -12,11 +12,11 @@ from sklearn.model_selection import train_test_split
 data_dir="./"
 raw_data_filename = data_dir + "kddcup.data"
 
-print "Loading raw data"
+print ("Loading raw data")
 
 raw_data = pd.read_csv(raw_data_filename, header=None)
 
-print "Transforming data"
+print ("Transforming data")
 # Categorize columns: "protocol", "service", "flag", "attack_type"
 raw_data[1], protocols= pd.factorize(raw_data[1])
 raw_data[2], services = pd.factorize(raw_data[2])
@@ -40,27 +40,27 @@ df= pd.DataFrame(features)
 # Note: train_size + test_size < 1.0 means we are subsampling
 # Use small numbers for slow classifiers, as KNN, Radius, SVC,...
 X_train, X_test, y_train, y_test = train_test_split(df, labels, train_size=0.8, test_size=0.2)
-print "X_train, y_train:", X_train.shape, y_train.shape
-print "X_test, y_test:", X_test.shape, y_test.shape
+print ("X_train, y_train:", X_train.shape, y_train.shape)
+print ("X_test, y_test:", X_test.shape, y_test.shape)
 
 
 # Training, choose model by commenting/uncommenting clf=
-print "Training model"
+print ("Training model")
 clf= RandomForestClassifier(n_jobs=-1, random_state=3, n_estimators=102)#, max_features=0.8, min_samples_leaf=3, n_estimators=500, min_samples_split=3, random_state=10, verbose=1)
 #clf = DecisionTreeClassifier(criterion='gini', splitter='best', max_depth=None, min_samples_split=2, min_samples_leaf=1, min_weight_fraction_leaf=0.0, max_features=None, random_state=None, max_leaf_nodes=None, min_impurity_decrease=0.0, class_weight=None, presort=False)
 
 trained_model= clf.fit(X_train, y_train)
 
-print "Score: ", trained_model.score(X_train, y_train)
+print ("Score: ", trained_model.score(X_train, y_train))
 
 # Predicting
-print "Predicting"
+print ("Predicting")
 y_pred = clf.predict(X_test)
 
-print "Computing performance metrics"
+print ("Computing performance metrics")
 results = confusion_matrix(y_test, y_pred)
 error = zero_one_loss(y_test, y_pred)
 
-print "Confusion matrix:\n", results
-print "Error: ", error
+print ("Confusion matrix:\n", results)
+print ("Error: ", error)
 
